ad8771781238c760fb532212292389b1eb05048d
ostrjul8 <studostr@gmail.com>
2026-04-13 19:34:45 +0300

    Delete unused models

--- ElectricityAPI/BLL/Models/DistrictDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BLL.Models
-{
-    public class DistrictDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- ElectricityAPI/BLL/Models/OpenChatRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace BLL.Models;
-
-public class OpenChatRequestDTO
-{
-    public bool IsNewChat { get; set; }
-    public int? ChatId { get; set; }
-    public string Text { get; set; } = string.Empty;
-}

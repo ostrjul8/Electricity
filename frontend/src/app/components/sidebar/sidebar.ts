0689f419adece9ba5cde18c577824dc9f30de30c
ostrjul8 <studostr@gmail.com>
2026-04-18 05:41:39 +0300

    Refactor sidebar component

--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
-import { NavItem } from "./nav-item/nav-item";
+import { Button } from "@shared/components/button/button";
 
 @Component({
     selector: "app-sidebar",
-    imports: [NavItem],
+    imports: [Button],
     templateUrl: "./sidebar.html",
     styleUrl: "./sidebar.css",
 })
